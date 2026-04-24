01ae11bcb50ac6ecb9c0b9b769058b9ba9ff531c
Peterson <petersonros@hotmail.com>
2026-04-24 13:45:27 -0300

    novos links

--- modules/open.ps1
+++ modules/open.ps1
@@ -2,10 +2,8 @@ Write-Host "🚀 Abrindo URLs da aula..." -ForegroundColor Cyan
 
 # URLs da aula
 $urls = @(
-    "https://wordwall.net/pt/resource/18749562/quebra-cabe%C3%A7a-animais-dom%C3%A9sticos",
-    "https://www.digipuzzle.net/digipuzzle/kids/puzzles/linkpuzzle_alphabet_pt.htm?language=portuguese&linkback=../../../pt/jogoseducativos/alfabeto/index.htm",
-    "https://www.digipuzzle.net/digipuzzle/kids/puzzles/linkpuzzle_animals_shadows.htm?language=portuguese&linkback=../../../pt/jogoseducativos/infantil/index.htm",
-    "https://wordwall.net/pt/resource/54499719/formas-geom%C3%A9tricas"
+    "https://www.digipuzzle.net/digipuzzle/kids/puzzles/letterbattle_pt.htm?language=portuguese&linkback=../../../pt/jogoseducativos/alfabeto/index.htm",
+    "https://www.digipuzzle.net/digipuzzle/animals/puzzles/clutter.htm?language=portuguese&linkback=../../../pt/jogoseducativos/jogos/index.htm"
 )
 
 # Detectar navegador
