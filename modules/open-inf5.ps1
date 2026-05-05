e580881f5d80bbf8acccfa1ce37b44c22d5d0253
Peterson <petersonros@hotmail.com>
2026-05-05 16:34:04 -0300

    novos links

--- modules/open-inf5.ps1
+++ modules/open-inf5.ps1
@@ -3,7 +3,8 @@ Write-Host "🚀 Abrindo URLs da aula — Infantil 5..." -ForegroundColor Cyan
 # URLs da aula — Infantil 5
 $urls = @(
     "https://www.digipuzzle.net/minigames/bubblechain/bubblechain_alphabet.htm?language=portuguese&linkback=../../pt/jogoseducativos/alfabeto/index.htm",
-    "https://www.digipuzzle.net/digipuzzle/kids/puzzles/linkpuzzle_alphabet_pt.htm?language=portuguese&linkback=../../../pt/jogoseducativos/alfabeto/index.htm"
+    "https://www.digipuzzle.net/digipuzzle/kids/puzzles/linkpuzzle_alphabet_pt.htm?language=portuguese&linkback=../../../pt/jogoseducativos/alfabeto/index.htm",
+    "https://www.digipuzzle.net/kids/animalcartoons/puzzles/countit.htm?language=portuguese&linkback=../../../pt/jogoseducativos/index.htm"
 )
 
 # Detectar navegador
