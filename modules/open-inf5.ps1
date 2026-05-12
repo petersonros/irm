08cf3c8f1e5102f32a7e94553cccc461dd69c902
Peterson <petersonros@hotmail.com>
2026-05-12 16:02:31 -0300

    novos links

--- modules/open-inf5.ps1
+++ modules/open-inf5.ps1
@@ -2,7 +2,10 @@ Write-Host "🚀 Abrindo URLs da aula — Infantil 5..." -ForegroundColor Cyan
 
 # URLs da aula — Infantil 5
 $urls = @(
-    "https://www.escolagames.com.br/jogos/monta-monta-bicho"
+    "https://www.escolagames.com.br/jogos/monta-monta-bicho",
+   "https://www.escolagames.com.br/jogos/robo-pega-letras",
+   "https://www.escolagames.com.br/jogos/simetria-magica#goog_rewarded",
+   "https://www.escolagames.com.br/jogos/pesca-letras"
 )
 
 # Detectar navegador
