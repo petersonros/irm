500d947b54fa10164302af85b3392983c45226d8
Peterson <petersonros@hotmail.com>
2026-05-27 09:12:58 -0300

    edit links open.ps2

--- modules/open-inf2.ps1
+++ modules/open-inf2.ps1
@@ -2,8 +2,10 @@ Write-Host "🚀 Abrindo URLs da aula — Infantil 2..." -ForegroundColor Cyan
 
 # URLs da aula — Infantil 2
 $urls = @(
-    "https://wordwall.net/pt/resource/4044063/jogo-da-mem%C3%B3ria-animais",
-    "https://wordwall.net/pt/resource/32416546/educa%C3%A7%C3%A3o-e-treinamento/jogos-da-mem%C3%B3ria-animais"
+    "https://wordwall.net/pt/resource/103609701/jogo-de-mem%C3%B3ria-infantil",
+    "https://www.digipuzzle.net/minigames/findtheletter/findtheletter_pt.htm?language=portuguese&linkback=../../pt/jogoseducativos/alfabeto/index.htm",
+    "https://www.digipuzzle.net/digipuzzle/animals/puzzles/shapepuzzle.htm?language=portuguese&linkback=../../../pt/jogoseducativos/infantil/index.htm",
+    "https://www.digipuzzle.net/kids/animalcartoons/puzzles/swaprows.htm?language=portuguese&linkback=../../../pt/jogoseducativos/infantil/index.htm"
 )
 
 # Detectar navegador
