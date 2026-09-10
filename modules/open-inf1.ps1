3bbeaf97e33f6004bbb1ef7407f9146304a6c702
Peterson <petersonros@hotmail.com>
2026-09-10 08:03:00 -0300

    atualização dos links infantil 1

--- modules/open-inf1.ps1
+++ modules/open-inf1.ps1
@@ -4,9 +4,13 @@ Write-Host "🚀 Abrindo URLs da aula — Infantil 1..." -ForegroundColor Cyan
 $urls = @(
  #   "https://www.digipuzzle.net/digipuzzle/kids/puzzles/hiddenobjects.htm?language=portuguese&linkback=../../../pt/jogoseducativos/palavras/index.htm",
 #    "https://www.digipuzzle.net/minigames/codegrid/codegrid_images_shapes.htm?language=portuguese&linkback=../../pt/jogoseducativos/ciencias/index.htm"
-    "https://wordwall.net/pt/resource/4191449/m-ou-n-eccavp",
-    "https://www.digipuzzle.net/minigames/flashmath/finderrors_texts_pt_mn.htm?language=portuguese&linkback=../../pt/jogoseducativos/palavras/index.htm",
-    "https://www.digipuzzle.net/kids/animalcartoons/puzzles/photosearch.htm?language=portuguese&linkback=../../../pt/jogoseducativos/jogos/index.htm"
+ #   "https://wordwall.net/pt/resource/4191449/m-ou-n-eccavp",
+ #   "https://www.digipuzzle.net/minigames/flashmath/finderrors_texts_pt_mn.htm?language=portuguese&linkback=../../pt/jogoseducativos/palavras/index.htm",
+ #   "https://www.digipuzzle.net/kids/animalcartoons/puzzles/photosearch.htm?language=portuguese&linkback=../../../pt/jogoseducativos/jogos/index.htm"
+    "https://wordwall.net/pt/resource/116240486/leitura-de-frases",
+    "https://wordwall.net/pt/resource/4081465/leitura",
+    "https://wordwall.net/pt/resource/16831120/leitura-de-frases-1ano",
+    "https://wordwall.net/pt/resource/77901051/leitura/interpreta%C3%A7%C3%A3o-de-frases"
 )
 
 # Detectar navegador
