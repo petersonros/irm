af6a172c215db9f52d2b60d6cf69f9605819d0c1
Peterson <petersonros@hotmail.com>
2026-09-24 15:18:56 -0300

    update infantil 4

--- modules/open-inf4.ps1
+++ modules/open-inf4.ps1
@@ -2,17 +2,24 @@ Write-Host "🚀 Abrindo URLs da aula — Infantil 4..." -ForegroundColor Cyan
 
 # URLs da aula — Infantil 4
 $urls = @(
-    "https://wordwall.net/pt/resource/25820616/jogo-da-mem%C3%B3ria-animais",
-    "https://wordwall.net/pt/resource/17128941/jogo-da-mem%C3%B3ria-dinossauro",
-    "https://wordwall.net/pt/resource/17070730/percep%C3%A7%C3%A3o-e-mem%C3%B3ria/jogo-da-mem%C3%B3ria-s%C3%ADtio-do",
-    "https://wordwall.net/pt/resource/4044063/jogo-da-mem%C3%B3ria-animais",
-    "https://wordwall.net/pt/resource/32416546/educa%C3%A7%C3%A3o-e-treinamento/jogos-da-mem%C3%B3ria-animais",
-    "https://www.digipuzzle.net/minigames/draw/mysterypictures_alphabet.htm?language=portuguese&linkback=../../pt/jogoseducativos/alfabeto/index.htm",
-    "https://www.digipuzzle.net/animals/monkeys/puzzles/puzzle_jigsaw.htm?language=english&linkback=../../../main/animals/index.htm",
-    "https://www.digipuzzle.net/digipuzzle/animals/puzzles/puzzle_jigsaw.htm?language=english&linkback=../../../main/animals/index.htm",
+    # "https://wordwall.net/pt/resource/25820616/jogo-da-mem%C3%B3ria-animais",
+    # "https://wordwall.net/pt/resource/17128941/jogo-da-mem%C3%B3ria-dinossauro",
+    # "https://wordwall.net/pt/resource/17070730/percep%C3%A7%C3%A3o-e-mem%C3%B3ria/jogo-da-mem%C3%B3ria-s%C3%ADtio-do",
+    # "https://wordwall.net/pt/resource/4044063/jogo-da-mem%C3%B3ria-animais",
+    # "https://wordwall.net/pt/resource/32416546/educa%C3%A7%C3%A3o-e-treinamento/jogos-da-mem%C3%B3ria-animais",
+    # "https://www.digipuzzle.net/minigames/draw/mysterypictures_alphabet.htm?language=portuguese&linkback=../../pt/jogoseducativos/alfabeto/index.htm",
+    # "https://www.digipuzzle.net/animals/monkeys/puzzles/puzzle_jigsaw.htm?language=english&linkback=../../../main/animals/index.htm",
+    # "https://www.digipuzzle.net/digipuzzle/animals/puzzles/puzzle_jigsaw.htm?language=english&linkback=../../../main/animals/index.htm",
+    # "https://wordwall.net/pt/resource/4980262/letras-e-n%C3%BAmeros",
+    # "https://wordwall.net/pt/resource/21805313/n%C3%BAmeros-letras-e-objetos"
 
-    "https://wordwall.net/pt/resource/4980262/letras-e-n%C3%BAmeros",
-    "https://wordwall.net/pt/resource/21805313/n%C3%BAmeros-letras-e-objetos"
+    # 24-09-2026 (Dione Infantil 4)
+    "https://wordwall.net/pt/resource/17128941/jogo-da-mem%C3%B3ria-dinossauro",
+    "https://www.digipuzzle.net/minigames/draw/mysterypictures_alphabet.htm?language=portuguese&linkback=../../pt/jogoseducativos/alfabeto/index.htm#google_vignette",
+    "https://www.digipuzzle.net/digipuzzle/animals/puzzles/puzzle_jigsaw.htm?language=english&linkback=../../../main/animals/index.htm#google_vignette",
+    "https://www.digipuzzle.net/digipuzzle/autumn/puzzles/lines.htm?language=english&linkback=../../../education/autumn/index.htm",
+    "https://www.digipuzzle.net/minigames/decorate/purplehorse/purplehorse.htm?language=english&linkback=../../../education/games/index.htm",
+    "https://www.digipuzzle.net/minigames/decorate/potatoheads/potatoheads.htm?language=english&linkback=../../../education/games/index.htm"
 
 )
 
